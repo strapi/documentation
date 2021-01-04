4ba75185e5c3d6cf513b4ca4a59545a5b8163110
Mégane Lacheny <megane.lacheny@strapi.io>
2021-01-04 18:32:40 +0100

    Reorganise backend customization file

--- developer-docs/.vuepress/config.js
+++ developer-docs/.vuepress/config.js
@@ -265,7 +265,7 @@ module.exports = {
           children: [
             ['/latest/setup-deployment-guides/installation.md', 'Installation'],
             ['/latest/setup-deployment-guides/framework-configurations.md', 'Framework configuration'],
-            ['/latest/setup-deployment-guides/customization.md', 'Customization'],
+            ['/latest/setup-deployment-guides/customization.md', 'Backend customization'],
             ['/latest/setup-deployment-guides/deployment.md', 'Deployment'],
           ]
         },
