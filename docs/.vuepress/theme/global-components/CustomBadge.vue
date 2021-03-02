cd78ccf8e176884db0f286a38e98e259dc6a6c92
Pierre Wizla <pwizla+github@gmail.com>
2021-03-02 16:29:12 +0100

    Add CustomBadge and GoldBadge components and use them in existing docs

---
+++ docs/.vuepress/theme/global-components/CustomBadge.vue
@@ -0,0 +1,44 @@
+<template>
+ <a :href="link" v-if="link"><Badge :type=thisType :text=text vertical="middle"/><OutboundLink  v-if="withLinkIcon"/></a>
+ <span v-else>
+  <Badge :type=thisType :text=text vertical="middle"/><OutboundLink  v-if="withLinkIcon"/>
+ </span>
+</template>
+
+<script>
+export default {
+  name: 'CustomBadge',
+  props: {
+    text: {
+      type: String,
+      default: "Badge",
+      required: true
+    },
+    withLinkIcon: {
+      type: Boolean,
+    },
+    link: {
+      type: String,
+    },
+    type: {
+      type: String,
+    }
+  },
+  computed: {
+    thisType() {
+      /** Eventually we could extend this to override badge background color.
+       * Currently the built-in VuePress badge only accepts three types,
+       * each of them being associated with a color:
+       *   - type: tip     => green badge
+       *   - type: warning => yellow badge
+       *   - type: error   => red badge
+       */
+
+      if (this.type === 'gold') {
+        return "warning"
+      }
+      return this.type
+    }
+  },
+}
+</script>
