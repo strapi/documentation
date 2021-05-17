b3e09d026a19f55b4ed64a2dfb4d884298f460f8
Pierre Wizla <pwizla+github@gmail.com>
2021-05-17 16:01:03 +0200

    Improve CSS for InstallLinkAlt components

--- docs/.vuepress/theme/global-components/InstallLinkAlt.vue
+++ docs/.vuepress/theme/global-components/InstallLinkAlt.vue
@@ -1,10 +1,10 @@
 <template>
 	<router-link :to="link" class="install-link install-link--alt">
     <span class="install-link__container">
-      <span class="icon">
+      <span class="icon__wrapper">
         <slot name="icon" />
       </span>
-      <span>
+      <span class="title__wrapper">
         <p class="title"><slot name="title" /></p>
 			<!-- <p class="description"><slot name="description" /></p> -->
       </span>
@@ -27,7 +27,8 @@
 
 <style lang="scss" scoped>
 .install-link {
-	background-color: rgba(63,74,86, .6);
+  // background-color: rgba(56,66,78,.6);
+  background-color: rgba(56,66,78, .2);
 	display: flex;
   flex-direction: column;
 	align-items: center;
@@ -39,30 +40,39 @@
   height: 100px;
 	margin: 16px 0; // You'd normally do layout margins etc from the parent, but seeing the parent
 					// is a markdown file, that's a bit annoying
-  text-decoration: none;
-  transition: background-color ease-out .4s;
+  text-decoration: none !important;
+  transition: background-color ease-out .2s;
 
   &__container {
     display: flex;
     flex-direction: column;
+    height: 60px;
   }
 
-	.icon {
+	.icon__wrapper {
 		flex-shrink: 0;
-		width: 40px;
+		width: 60px;
+    height: 60px;
     margin-left: auto;
     margin-right: auto;
+    transition: all ease-out .2s;
+
 		& ::v-deep svg {
       width: 100%;
 			height: auto;
 		}
 	}
+
 	.title {
-		// font-weight: 700;
 		margin: 0;
     text-align: center;
 
+    &__wrapper {
+      opacity: 0;
+      transition: opacity ease-out .2s;
+    }
 	}
+
 	.description {
 		font-size: 12px;
 		margin: 0;
@@ -70,6 +80,19 @@
 
   &:hover {
     background-color: rgb(129,107,250);
+
+    .title__wrapper {
+      opacity: 1;
+    }
+
+    .title {
+      text-decoration: none;
+    }
+
+    .icon__wrapper {
+      width: 40px;
+      height: 44px;
+    }
   }
 }
 </style>
