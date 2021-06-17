2a599023fc3e8f06563a8419c60a3b7091f80db7
Pierre Wizla <pwizla+github@gmail.com>
2021-06-17 16:12:49 +0200

    Reduce Ruby + Flutter logo sizes

--- docs/.vuepress/theme/global-components/IntegrationLinksAlt.vue
+++ docs/.vuepress/theme/global-components/IntegrationLinksAlt.vue
@@ -632,9 +632,9 @@
             xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
             version="1.1"
             id="Layer_1"
-            width="198.13"
-            height="197.58"
-            viewBox="0 0 198.13 197.58"
+            width="72"
+            height="72"
+            viewBox="-10 -10 230 230"
             overflow="visible"
             enable-background="new 0 0 198.13 197.58"
             xml:space="preserve"
@@ -1329,7 +1329,13 @@
     <div class="install-link__wrapper">
       <InstallLinkAlt link="/developer-docs/latest/developer-resources/content-api/integrations/flutter.html">
         <template #icon>
-          <svg enable-background="new 0 0 1999 2474.2" viewBox="0 0 2500 2474.2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><linearGradient id="a" gradientTransform="matrix(380.4042 -380.4175 -53.104 -53.1021 908228.3125 -700059.5)" gradientUnits="userSpaceOnUse" x1="-2115.0029" x2="-2115.0029" y1="1930.7407" y2="1929.7407"><stop offset=".2" stop-opacity=".15"/><stop offset=".85" stop-color="#616161" stop-opacity=".01"/></linearGradient><linearGradient id="b" gradientTransform="matrix(565.291 0 0 -380.9571 1196694.25 743476.5625)" gradientUnits="userSpaceOnUse" x1="-2115.2688" x2="-2114.2688" y1="1946.3563" y2="1946.3563"><stop offset=".2" stop-opacity=".55"/><stop offset=".85" stop-color="#616161" stop-opacity=".01"/></linearGradient><path d="m381 1618-381-381 1237.1-1237h761.9m0 1141.5h-761.9l-285.4 285.4 381 381" fill="#42a5f5" fill-opacity=".8"/><path d="m951.7 2188.8 285.4 285.4h761.9l-666.3-666.3" fill="#0d47a1"/><path d="m571.6 1808.1 380.4-380.5 380.4 380.4-380.4 380.5z" fill="#42a5f5"/><path d="m952 2188.5 380.4-380.4 53.1 53.1-380.4 380.4z" fill="url(#a)"/><path d="m951.7 2188.8 565.3-195.3-184.3-185.7" fill="url(#b)"/></svg>
+          <svg
+            enable-background="new 0 0 1999 2474"
+            viewBox="-50 -40 2700 2674"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+          >
+          <linearGradient id="a" gradientTransform="matrix(380.4042 -380.4175 -53.104 -53.1021 908228.3125 -700059.5)" gradientUnits="userSpaceOnUse" x1="-2115.0029" x2="-2115.0029" y1="1930.7407" y2="1929.7407"><stop offset=".2" stop-opacity=".15"/><stop offset=".85" stop-color="#616161" stop-opacity=".01"/></linearGradient><linearGradient id="b" gradientTransform="matrix(565.291 0 0 -380.9571 1196694.25 743476.5625)" gradientUnits="userSpaceOnUse" x1="-2115.2688" x2="-2114.2688" y1="1946.3563" y2="1946.3563"><stop offset=".2" stop-opacity=".55"/><stop offset=".85" stop-color="#616161" stop-opacity=".01"/></linearGradient><path d="m381 1618-381-381 1237.1-1237h761.9m0 1141.5h-761.9l-285.4 285.4 381 381" fill="#42a5f5" fill-opacity=".8"/><path d="m951.7 2188.8 285.4 285.4h761.9l-666.3-666.3" fill="#0d47a1"/><path d="m571.6 1808.1 380.4-380.5 380.4 380.4-380.4 380.5z" fill="#42a5f5"/><path d="m952 2188.5 380.4-380.4 53.1 53.1-380.4 380.4z" fill="url(#a)"/><path d="m951.7 2188.8 565.3-195.3-184.3-185.7" fill="url(#b)"/></svg>
         </template>
         <template #title>Flutter</template>
         <template #description>
@@ -1372,7 +1378,7 @@
             preserveAspectRatio="xMidYMid"
             version="1.1"
             height="48px"
-            viewBox="0 -50 255 185"
+            viewBox="0 -60 255 195"
             xmlns="http://www.w3.org/2000/svg">
           <defs>
           <radialGradient id="a" cx=".8366" cy="-125.81" r="363.06" gradientTransform="matrix(.463 0 0 .463 76.464 81.918)" gradientUnits="userSpaceOnUse">
