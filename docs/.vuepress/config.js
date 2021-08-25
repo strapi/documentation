337b5ca60dc2013ea40ad466786446034ce3d1ac
Pierre Wizla <pwizla+github@gmail.com>
2021-08-25 10:50:29 +0200

    Remove GTM script from plugins
    (it's already in head config and this has been fixed in main branch)

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -424,9 +424,6 @@ const plugins = [
     siteTitle: (_, $site) => $site.title,
     title: $page => $page.title,
   }],
-  ['script', {},
-    `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-KN9JRWG');`,
-  ],
   ['vuepress-plugin-code-copy', {
     color: '#ffffff',
     successText: 'Copied to clipboard!',
