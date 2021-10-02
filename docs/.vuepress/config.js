c4c537da39d3fd6dbf80e8ee7a97ee0e380dd074
Pierre Wizla <pwizla+github@gmail.com>
2021-10-02 19:52:32 +0200

    Create a Content API section

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -236,8 +236,15 @@ const sidebar = {
           collapsable: true,
           sidebarDepth: 1,
           children: [
-            ['/developer-docs/latest/developer-resources/database-apis-reference/rest-api.md', 'REST API'],
-            ['/developer-docs/latest/developer-resources/database-apis-reference/graphql-api.md', 'GraphQL API'],
+            {
+              title: 'Content API',
+              collapsable: true,
+              path: '/developer-docs/latest/developer-resources/database-apis-reference/content-api.html',
+              children: [
+                ['/developer-docs/latest/developer-resources/database-apis-reference/rest-api.md', 'REST API'],
+                ['/developer-docs/latest/developer-resources/database-apis-reference/graphql-api.md', 'GraphQL API'],
+              ]
+            },
             {
               title: 'Query Engine API',
               path: '/developer-docs/latest/developer-resources/database-apis-reference/query-engine-api.html',
