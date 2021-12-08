09fa00be910a27b432b1c2bf2966fbaec740f718
soupette <cyril@strapi.io>
2021-12-08 10:57:59 +0100

    Split sidebar
    Signed-off-by: soupette <cyril@strapi.io>

--- docs/scripts/create-config-file.js
+++ docs/scripts/create-config-file.js
@@ -5,29 +5,14 @@ const fs = require('fs-extra');
 // const sidebar = require('../.vuepress/sidebar');
 
 const createConfigFile = async () => {
-  const sidebar = await fs.readFile(path.resolve(__dirname, '..', '.vuepress', 'sidebar.js'));
+  const dest = path.resolve(__dirname, '..', '.vuepress');
+  const sidebar = await fs.readFile(path.resolve(dest, 'sidebar.js'));
+  const linkChecker = await fs.readFile(path.resolve(dest, 'link-checker.js'));
 
   const content = `
 ${sidebar.toString()}
-  
-const checklinksIgnoredFiles = [
-  '**/node_modules', // please never remove this one
-  /**
-   * Caution: Adding an individual file to this section
-   * will prevent the _whole_ file from being scanned for broken links.
-   *
-   * Currently, there is no easy way to ignore a specific link inside a file.
-   */
 
-  /**
-   * Files below give false positives
-   */
-  '/developer-docs/latest/developer-resources/plugin-api-reference/admin-panel.md',
-  './developer-docs/latest/development/backend-customization/models.md',
-  './developer-docs/latest/guides/count-graphql.md', // might be removed once GraphQL customization is ready
-  './developer-docs/latest/setup-deployment-guides/configurations.md', // the script thinks filename[]() at line 977 is a real link
-  './developer-docs/latest/development/backend-customization/webhooks.md', // 'missing" links are in commented part of file
-];
+${linkChecker.toString()}
 
 const plugins = [
   ['vuepress-plugin-element-tabs', {}],
