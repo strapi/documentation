b869090dd3f4ac851333ffdd908830f8cbad6cef
Pierre Wizla <pwizla+github@gmail.com>
2022-08-02 18:59:58 +0200

    Fix unclosed array in TOC config

--- docs/.vuepress/config/sidebar-developer.js
+++ docs/.vuepress/config/sidebar-developer.js
@@ -231,8 +231,7 @@ const developer = [
         // children: [
           // ['/developer-docs/latest/development/custom-fields/reference.md', 'Reference'],
           // ['/developer-docs/latest/development/custom-fields/color-custom-fields-guide.md', 'Color custom field example'],
-        ]
-
+        // ]
       },
       ['/developer-docs/latest/development/typescript.md', 'TypeScript'],
       ['/developer-docs/latest/development/providers.md', 'Providers'],
