5999f327ab96ff62a1bec1bfb542763b1c169df7
Pierre Wizla <pwizla+github@gmail.com>
2022-08-24 18:31:42 +0200

    Remove multimarkdown plugin

--- docs/.vuepress/config/markdown.js
+++ docs/.vuepress/config/markdown.js
@@ -2,10 +2,5 @@ const markdown = {
   extendMarkdown: md => {
     // use more markdown-it plugins!
     md.use(require('markdown-it-include'));
-    md.use(require('markdown-it-multimd-table'), {
-      multiline: true,
-      rowspan: true,
-      headerless: true
-    });
   },
 };
