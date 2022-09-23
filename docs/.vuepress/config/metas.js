89f88680ff0a5e7b08dedcf55ae71454fbd97ff7
Aurélien GEORGET <aurelsicoko@gmail.com>
2022-09-23 18:38:43 +0700

    Installing hotjar (#1061)
    * Support HTML in SidebarGroup title
    
    * Add hotjar
    
    * Delete SidebarGroup.vue

--- docs/.vuepress/config/metas.js
+++ docs/.vuepress/config/metas.js
@@ -100,6 +100,11 @@ const metas = {
       {},
       `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-KN9JRWG');`,
     ],
+    [
+      'script',
+      {},
+      `(function(h,o,t,j,a,r){h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};h._hjSettings={hjid:3105445,hjsv:6};a=o.getElementsByTagName('head')[0];r=o.createElement('script');r.async=1;r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;a.appendChild(r);})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+    ],
   ],
   extraWatchFiles: [
     '.vuepress/config/sidebar-developer.js',
