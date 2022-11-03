b7c25f713ecad012cdd33b6291deedbe299f9957
Pierre Wizla <pwizla+github@gmail.com>
2022-11-03 10:57:53 +0100

    Fix strapi callout for dark mode

--- docusaurus/src/theme/Admonition/Types.js
+++ docusaurus/src/theme/Admonition/Types.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import DefaultAdmonitionTypes from '@theme-original/Admonition/Types';
+import { useColorMode } from '@docusaurus/theme-common';
 
 function StrapiAdmonition(props) {
+  const { colorMode } = useColorMode();
+  const isDarkMode = colorMode === 'dark'
+
   return (
     // <div style={{border: 'solid red', padding: 10}}>
     //   <h5 style={{color: 'blue', fontSize: 30}}>{props.title}</h5>
     //   <div>{props.children}</div>
     // </div>
-    <div style={{backgroundColor: '#f0f0ff', borderLeftColor: '#4945ff'}} class="theme-admonition theme-admonition-caution admonition_node_modules-@docusaurus-theme-classic-lib-theme-Admonition-Layout-styles-module alert alert--warning admonition--strapi">
-      <div class="admonitionHeading_node_modules-@docusaurus-theme-classic-lib-theme-Admonition-Layout-styles-module">
-        <span style={{color: '#4945ff'}} class="admonitionIcon_node_modules-@docusaurus-theme-classic-lib-theme-Admonition-Layout-styles-module">
+    <div
+      style={isDarkMode ? {backgroundColor: '#7b79ff', borderLeftColor: '#4945ff'} : {backgroundColor: '#f0f0ff', borderLeftColor: '#4945ff'}}
+      className="theme-admonition theme-admonition-caution admonition_node_modules-@docusaurus-theme-classic-lib-theme-Admonition-Layout-styles-module alert alert--warning admonition--strapi">
+      <div className="admonitionHeading_node_modules-@docusaurus-theme-classic-lib-theme-Admonition-Layout-styles-module">
+        <span style={isDarkMode ? {color: '#FFF8E6'} : {color: '#4945ff'}} className="admonitionIcon_node_modules-@docusaurus-theme-classic-lib-theme-Admonition-Layout-styles-module">
           🤓 {props.title}
         </span>
       </div>
-      <div class="admonitionContent_node_modules-@docusaurus-theme-classic-lib-theme-Admonition-Layout-styles-module">
+      <div
+        className="admonitionContent_node_modules-@docusaurus-theme-classic-lib-theme-Admonition-Layout-styles-module"
+      >
         {props.children}
       </div>
     </div>
