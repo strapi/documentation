b734f60451c582312c5d9fd46960a02f70eeeb25
Pierre Wizla <pwizla+github@gmail.com>
2022-11-08 19:32:37 +0100

    Fix some package details

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -17,8 +17,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'pwizla', // Usually your GitHub org/user name.
-  projectName: 'strapi-docs-docusaurus-poc', // Usually your repo name.
+  organizationName: 'strapi', // Usually your GitHub org/user name.
+  projectName: 'documentation', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
