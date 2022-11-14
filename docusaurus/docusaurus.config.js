d37c5856db73888774e5ddfa554295328ff0cf76
Pierre Wizla <pwizla+github@gmail.com>
2022-11-14 18:49:10 +0100

    Remove copyright line from footer

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -175,7 +175,6 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
