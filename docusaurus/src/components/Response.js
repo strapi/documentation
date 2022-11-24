fe96bc0890392d80951795949856710ad2b84b64
Pierre Wizla <pwizla+github@gmail.com>
2022-11-24 15:16:56 +0100

    Move api-calls CSS to separate file and add default title to components

--- docusaurus/src/components/Response.js
+++ docusaurus/src/components/Response.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 export default function Response(props) {
+  const { title = 'Example response '} = props;
   return (
       <div className="response">
-        <div className="response-title">{ props.title }</div>
+        <div className="response-title">{ title }</div>
         <div className="response-content">{ props.children }</div>
       </div>
   );
