04b0e3b4faa1fd5c93947fadbb5f246cfd354eaf
Pierre Wizla <pwizla+github@gmail.com>
2023-02-17 17:22:44 +0100

    Disable the beta docs announcement bar

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -77,13 +77,13 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      announcementBar: {
-        id: 'support_us',
-        content: "We're rebuilding our documentation from the ground up (see <a href='/'>status page</a>). Looking for the complete Strapi documentation? Please visit <a target='_blank' rel='noopener noreferrer' href='https://docs.strapi.io'>docs.strapi.io</a>.",
-        backgroundColor: '#F3E5FA',
-        textColor: '#091E42',
-        isCloseable: true,
-      },
+      // announcementBar: {
+      //   id: 'support_us',
+      //   content: "We're rebuilding our documentation from the ground up (see <a href='/'>status page</a>). Looking for the complete Strapi documentation? Please visit <a target='_blank' rel='noopener noreferrer' href='https://docs.strapi.io'>docs.strapi.io</a>.",
+      //   backgroundColor: '#F3E5FA',
+      //   textColor: '#091E42',
+      //   isCloseable: true,
+      // },
       docs: {
         sidebar: {
           hideable: true
