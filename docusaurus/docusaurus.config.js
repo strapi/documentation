aae6090022ca5c56faf8548f9362f23ef114e117
Pierre Wizla <pwizla+github@gmail.com>
2023-03-13 16:03:26 +0100

    Set up crawler to re-crawl everyday at midnight

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -135,6 +135,7 @@ const config = {
         appId: '392RJ63O14',
         apiKey: '8cf63dc4a24ff4087407f9f5bd188aae',
         indexName: 'strapiDocsNextstrapiDocsNext',
+        schedule: 'every 1 day at 0:00 am',
       },
       navbar: {
         logo: {
