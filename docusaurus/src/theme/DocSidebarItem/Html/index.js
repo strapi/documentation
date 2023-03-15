4a6e630326e7855fea2be85dbfd09dd0092b7d4e
Udi Silvestre <udi.silvestre@strapi.io>
2023-03-15 18:51:37 -0300

    Add 'New ✨' badge support to Sidebar items via customProps.new

---
+++ docusaurus/src/theme/DocSidebarItem/Html/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import clsx from 'clsx';
+import {ThemeClassNames} from '@docusaurus/theme-common';
+import styles from './styles.module.css';
+import { NewBadge } from '../../../components/Badge';
+
+export default function DocSidebarItemHtml({ item, level, index }) {
+  const {
+    value,
+    defaultStyle,
+    className,
+    customProps,
+  } = item;
+
+  return (
+    <li
+      className={clsx(
+        ThemeClassNames.docs.docSidebarItemLink,
+        ThemeClassNames.docs.docSidebarItemLinkLevel(level),
+        defaultStyle && [styles.menuHtmlItem, 'menu__list-item'],
+        className,
+      )}
+      key={index}
+    >
+      <span
+        className="menu__list-item__content"
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{__html: value}}
+      />
+      {customProps?.new && <NewBadge />}
+    </li>
+  );
+}
