ecd5c65ded477707ecaaf0c93fbfce1a62ebe480
Pierre Wizla <pwizla+github@gmail.com>
2023-09-20 21:28:52 +0200

    Improve code for SubtleCallout style

--- docusaurus/src/components/SubtleCallout.js
+++ docusaurus/src/components/SubtleCallout.js
@@ -6,15 +6,7 @@ export default function SubtleCallout({
   emoji = '🤓',
 }) {
   return (
-    <div
-      className="theme-admonition theme-admonition--callout"
-      style={{
-        borderRadius: '8px',
-        border: 'none',
-        backgroundColor: 'transparent',
-        border: 'solid 1px #8e8ea9',
-        marginLeft: '80px',
-      }}>
+    <div className="theme-admonition theme-admonition--callout theme-admonition--subtle">
       <div className="theme-admonition__heading">
         <span style={{fontWeight: 300}} className="theme-admonition__heading__icon">{ emoji } </span>{ title }
       </div>
