a51194f5a249d905af7eb9155f120010f36fbed1
Pierre Wizla <pwizla+github@gmail.com>
2024-01-19 16:29:09 +0100

    Add Plugin CLI docs to TOC

--- docusaurus/sidebars.js
+++ docusaurus/sidebars.js
@@ -452,6 +452,7 @@ const sidebars = {
               id: 'dev-docs/api/plugins/server-api',
               label: 'Server API',
             },
+            'dev-docs/plugins/development/plugin-cli',
             'dev-docs/custom-fields',
             'dev-docs/plugins-extension',
             {
@@ -464,6 +465,13 @@ const sidebars = {
               items: [
                 'dev-docs/plugins/guides/store-and-access-data',
                 'dev-docs/plugins/guides/pass-data-from-server-to-admin',
+                {
+                  type: 'doc',
+                  id: 'dev-docs/plugins/guides/use-the-plugin-cli',
+                  customProps: {
+                    new: true,
+                  }
+                }
               ]
             }
           ]
