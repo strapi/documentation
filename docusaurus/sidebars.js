c5353e527d7bb016268e16fb13b0fa4b7f1015c3
Pierre Wizla <pwizla+github@gmail.com>
2024-03-01 11:32:08 +0100

    Make guides section not collapsable in sidebar

--- docusaurus/sidebars.js
+++ docusaurus/sidebars.js
@@ -987,6 +987,7 @@ const sidebars = {
     {
       type: "category",
       label: "Rest API guides",
+      collapsed: false,
       link: {
         type: 'doc',
         id: 'dev-docs/api/rest/guides/intro',
