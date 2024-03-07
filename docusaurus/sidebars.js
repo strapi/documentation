e4742cdc02a3eb23334031d0985f987187f01e58
Pierre Wizla <pwizla+github@gmail.com>
2024-03-07 22:02:32 +0100

    Uncollapse v5 migration

--- docusaurus/sidebars.js
+++ docusaurus/sidebars.js
@@ -542,7 +542,7 @@ const sidebars = {
         // },
         {
           type: 'category',
-          collapsed: true,
+          collapsed: false,
           link: {
             type: 'doc',
             id: 'dev-docs/migration/v4-to-v5/introduction'
