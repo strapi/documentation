89531f516abd376da4535f173e2cb207e740c6ec
Pierre Wizla <pwizla+github@gmail.com>
2024-03-07 22:00:49 +0100

    Hide v5.x migration guides

--- docusaurus/sidebars.js
+++ docusaurus/sidebars.js
@@ -535,11 +535,11 @@ const sidebars = {
           },
         },
         'dev-docs/update-version',
-        {
-          type: 'doc',
-          label: 'v5.x migration guides',
-          id: 'dev-docs/migration-guides',
-        },
+        // {
+        //   type: 'doc',
+        //   label: 'v5.x migration guides',
+        //   id: 'dev-docs/migration-guides',
+        // },
         {
           type: 'category',
           collapsed: true,
