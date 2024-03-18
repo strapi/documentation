6d94cbc7720c871d424fd249ddafe9b0f62adb6f
Pierre Wizla <pwizla@users.noreply.github.com>
2024-03-18 14:20:01 +0100

    Document the "localizations field removed" breaking change (#2050)

--- docusaurus/sidebars.js
+++ docusaurus/sidebars.js
@@ -1116,6 +1116,7 @@ const sidebars = {
                 'dev-docs/migration/v4-to-v5/breaking-changes/draft-and-publish-always-enabled',
                 'dev-docs/migration/v4-to-v5/breaking-changes/sort-by-id',
                 'dev-docs/migration/v4-to-v5/breaking-changes/no-find-page-in-document-service',
+                'dev-docs/migration/v4-to-v5/breaking-changes/no-localizations-field',
               ]
             },
             {
