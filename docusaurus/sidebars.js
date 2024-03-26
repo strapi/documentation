2a305b588c1f465fb8d6a2ca0fb66987fe572449
Pierre Wizla <pwizla+github@gmail.com>
2024-03-26 09:52:07 +0100

    Reword GraphQL API changes

--- docusaurus/sidebars.js
+++ docusaurus/sidebars.js
@@ -1128,7 +1128,7 @@ const sidebars = {
                 'dev-docs/migration/v4-to-v5/breaking-changes/no-localizations-field',
                 'dev-docs/migration/v4-to-v5/breaking-changes/locale-attribute-reserved',
                 'dev-docs/migration/v4-to-v5/breaking-changes/components-and-dynamic-zones-do-not-return-id',
-                'dev-docs/migration/v4-to-v5/breaking-changes/graphql-api-rewritten',
+                'dev-docs/migration/v4-to-v5/breaking-changes/graphql-api-updated',
               ]
             },
             {
