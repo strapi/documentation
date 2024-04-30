1e9bef26f9fdacb0a402e285f9a7a70a36520598
Pierre Wizla <pwizla+github@gmail.com>
2024-04-30 11:37:30 +0200

    Create breaking change entry

--- docusaurus/sidebars.js
+++ docusaurus/sidebars.js
@@ -1158,6 +1158,7 @@ const sidebars = {
                 // 'dev-docs/migration/v4-to-v5/breaking-changes/components-and-dynamic-zones-do-not-return-id', // not implemented yet
                 'dev-docs/migration/v4-to-v5/breaking-changes/graphql-api-updated',
                 'dev-docs/migration/v4-to-v5/breaking-changes/entity-service-deprecated',
+                'dev-docs/migration/v4-to-v5/breaking-changes/default-input-validation',
               ]
             },
             {
