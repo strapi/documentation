5819d7bbddbac07c81ecb3db1f63a8e0182c6543
Pierre Wizla <pwizla@users.noreply.github.com>
2024-06-11 15:32:45 +0200

    Update docusaurus.config.js

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -280,7 +280,7 @@ const config = {
       "posthog-docusaurus",
       {
         apiKey: "phc_DETCfjmHIm184mceL8duwutdQgOnVTZSC59xontNRWN",
-        // appUrl: "<ph_client_api_host>", // optional, defaults to "https://us.i.posthog.com"
+        appUrl: "https://us.i.posthog.com", // optional, defaults to "https://us.i.posthog.com"
         enableInDevelopment: false, // optional
       },
     ],
