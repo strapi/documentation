f203702026de5c257908608e37be49bdbabed53b
Pierre Wizla <pwizla+github@gmail.com>
2024-08-06 18:50:38 +0200

    Fix cloud docs homepage link

--- docusaurus/src/pages/home/_home.content.js
+++ docusaurus/src/pages/home/_home.content.js
@@ -115,7 +115,7 @@ export default {
       ],
     },
     {
-      cardLink: '/cloud-docs/intro',
+      cardLink: '/cloud/intro',
       cardTitle: 'Cloud Documentation',
       cardDescription: 'Learn how to deploy and manage projects on Strapi Cloud',
       cardImgSrc: require('@site/static/img/assets/home/preview--cloud-docs.png').default,
