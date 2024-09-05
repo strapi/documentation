fc8e449c866b5c75d3c86a57c4c4ff09d4ad8fc0
Pierre Wizla <pwizla+github@gmail.com>
2024-09-05 14:51:25 +0200

    Improve carousel

--- docusaurus/src/pages/home/_home.content.js
+++ docusaurus/src/pages/home/_home.content.js
@@ -7,6 +7,22 @@ export default {
   },
   carousel: [
     {
+      title: 'Can’t wait to use Strapi?',
+      description: (
+        <>
+          {'Learn Strapi in a nutshell with our '}
+          <a href="https://docs.strapi.io/dev-docs/quick-start" target="_blank">quick start guide</a>
+          {', or request a '}
+          <a href="https://strapi.io/demo" target="_blank">live demo</a>!
+        </>
+      ),
+      button: {
+        label: 'Quick start',
+        decorative: '🚀',
+        to: '/dev-docs/quick-start',
+      },
+    },
+    {
       backgroundImgSrc: require('@site/static/img/assets/home/carousel-background--cloud.png').default,
       variant: 'cloud',
       title: 'Strapi Cloud',
@@ -22,22 +38,6 @@ export default {
         label: 'Strapi Cloud docs',
         decorative: '☁️',
         to: '/cloud/intro',
-      },
-    },
-    {
-      title: 'Can’t wait to use Strapi?',
-      description: (
-        <>
-          {'If demos are more your thing, we have a '}
-          <a href="https://youtu.be/h9vETeRiulY" target="_blank">video demo</a>
-          {', or you can request a '}
-          <a href="https://strapi.io/demo" target="_blank">live demo</a>!
-        </>
-      ),
-      button: {
-        label: 'Quick start',
-        decorative: '🚀',
-        to: '/dev-docs/quick-start',
       },
     },
   ],
