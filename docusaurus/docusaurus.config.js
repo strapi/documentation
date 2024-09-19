4fbdddcef19fe5754c3c52d2db9247f21be73d6d
Pierre Wizla <pwizla+github@gmail.com>
2024-09-19 10:55:54 +0200

    Revert "Update Algolia index name"
    This reverts commit 78219659de04b5f44ca12879e4f2a1ed1aa508f9.

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -152,7 +152,7 @@ const config = {
       algolia: {
          appId: '392RJ63O14',
          apiKey: 'ed62374a794e8da5accb298e13618614',
-         indexName: 'strapiDocsNextstrapiDocsNext',
+         indexName: 'strapiDocsv5beta',
       },
       navbar: {
         hideOnScroll: false,
