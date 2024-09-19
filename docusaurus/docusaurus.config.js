ab75974a8848bf02093f2f11918a937e9f250f83
Pierre Wizla <pwizla+github@gmail.com>
2024-09-19 11:13:31 +0200

    Try to set up another old Algolia index 🤞

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -147,7 +147,7 @@ const config = {
       algolia: {
         appId: '392RJ63O14',
         apiKey: 'ed62374a794e8da5accb298e13618614',
-        indexName: 'strapi',
+        indexName: 'strapiDocsBackup',
       },
       navbar: {
         hideOnScroll: false,
