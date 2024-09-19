fcb673092e4e213c693019fb8f9c30a53cd933ab
Pierre Wizla <pwizla+github@gmail.com>
2024-09-19 11:31:50 +0200

    Swapped Algolia for local search plugin

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -266,6 +266,7 @@ const config = {
     'docusaurus-plugin-hubspot',
     'docusaurus-plugin-sass',
     'docusaurus-plugin-image-zoom',
+    "@cmfcmf/docusaurus-search-local",
     /**
      * Seems like we have an issue where a medium-zoom--hidden class is applied on the second, top-most (z-index wise) image,
      * actually hiding the image when zoomed in. Found no related issue in the plugin's repo, might have to dig whether it's
