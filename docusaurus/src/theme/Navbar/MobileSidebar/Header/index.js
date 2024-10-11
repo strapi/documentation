460c8278e30de63096dbacf50756cf20f96f3e9c
Pierre Wizla <pwizla+github@gmail.com>
2024-10-11 17:30:48 +0200

    Finish implementing custom mobile navbar (code + styles)

--- docusaurus/src/theme/Navbar/MobileSidebar/Header/index.js
+++ docusaurus/src/theme/Navbar/MobileSidebar/Header/index.js
@@ -6,6 +6,8 @@ export default function HeaderWrapper(props) {
   return (
     <>
       <Header {...props} />
+      <a className="custom-mobile-navbar__item" href="/dev-docs/intro"><i className="ph-fill ph-feather"></i>CMS</a>
+      <a className="custom-mobile-navbar__item" href="/cloud/intro"><i className="ph-fill ph-cloud"></i>Cloud</a>
       <CustomSearchBar />
     </>
   );
