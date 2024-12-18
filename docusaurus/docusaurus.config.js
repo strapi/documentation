b3bfd30b72109ee11c78000cd8fc05208febd824
Pierre Wizla <pwizla+github@gmail.com>
2024-12-18 14:49:46 +0100

    Change Algolia index info

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -175,10 +175,10 @@ const config = {
         },
       },
       algolia: {
-         appId: '392RJ63O14',
-         apiKey: 'ed62374a794e8da5accb298e13618614',
-         indexName: 'strapiDocsv5beta',
-      },
+        appId: '392RJ63O14',
+        apiKey: 'ed62374a794e8da5accb298e13618614',
+        indexName: 'strapiDocsNextstrapiDocsNext',
+     },
       navbar: {
         hideOnScroll: false,
         logo: {
