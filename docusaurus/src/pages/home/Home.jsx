899b0408b1d660cef904b572d48ede307956d341
Pierre Wizla <pwizla+github@gmail.com>
2025-01-07 17:48:23 +0100

    Remove Dev Docs from homepage and create "CMS Docs" category there

--- docusaurus/src/pages/home/Home.jsx
+++ docusaurus/src/pages/home/Home.jsx
@@ -141,7 +141,7 @@ export default function PageHome() {
                     key={`pageHomeCategoryItem${categoryItemIndex}`}
                     className={clsx(
                       'col',
-                      'col--4',
+                      'col--6',
                       styles.home__categories__item,
                     )}
                   >
