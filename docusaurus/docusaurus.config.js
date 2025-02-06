fa9d481c10e9ebf0fb7548e9faef684e4433dc2c
Pierre Wizla <pwizla+github@gmail.com>
2025-02-06 15:22:01 +0100

    Fix typo in previous config. fix 😅

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -110,7 +110,7 @@ const config = {
           routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           editUrl: 'https://github.com/strapi/documentation/edit/v6/main/docusaurus', // TODO change back to next then main for production releases          admonitions: {
-          admonition: {
+          admonitions: {
             keywords: [
               // Admonitions defaults
               'note',
