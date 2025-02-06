1bbfbe02ba1239dc8316071d23e87d8ef5d857e3
Pierre Wizla <pwizla@users.noreply.github.com>
2025-02-06 11:59:35 +0100

    Update docusaurus.config.js

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -110,7 +110,7 @@ const config = {
         docs: {
           routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/strapi/documentation/edit/main/docusaurus',
+          editUrl: 'https://github.com/strapi/documentation/edit/v6/main/docusaurus', // TODO change back to next then main for production releases
           admonitions: {
             keywords: [
               // Admonitions defaults
