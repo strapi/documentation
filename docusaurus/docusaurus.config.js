f012170c85679b34f39c8b3f26da9bcdcce5051f
Pierre Wizla <pwizla+github@gmail.com>
2025-02-17 17:15:58 +0100

    Try with old (pre-CMS docs) Algolia index

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -167,7 +167,7 @@ const config = {
       algolia: {
          appId: '392RJ63O14',
          apiKey: 'ed62374a794e8da5accb298e13618614',
-         indexName: 'crawler_strapi_cmsDocs',
+         indexName: 'strapiDocsNextstrapiDocsNext',
          insights: true,
       },
       navbar: {
