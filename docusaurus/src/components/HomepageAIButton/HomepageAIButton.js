64e3c76ae6f76c04dfb6d2c617c47c3ac8cbc52a
Pierre Wizla <pwizla+github@gmail.com>
2025-03-12 14:42:58 +0100

    Restore missing trigger of AI modal

--- docusaurus/src/components/HomepageAIButton/HomepageAIButton.js
+++ docusaurus/src/components/HomepageAIButton/HomepageAIButton.js
@@ -5,7 +5,7 @@ import Icon from '../Icon'
 export default function HomepageAIButton() {
   return (
     <div className={styles['homepage-ai-button-container']}>
-      <button className={styles['homepage-ai-button']}>
+      <button className={`${styles['homepage-ai-button']} kapa-widget-button`}>
         <Icon name="sparkle" />
         <p>Ask anything to AI</p>
       </button>
