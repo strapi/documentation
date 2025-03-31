57aff84ad72aaea634f2ddc9aa29f2df565121f4
Pierre Wizla <pwizla+github@gmail.com>
2025-03-31 19:01:12 +0200

    Re-roll AI-based search
    All my previous attempts with Algolia failed, so I'm restoring the new
    AI-based search. Even if searches are slower, at least they work…

--- docusaurus/src/theme/DocSidebar/CustomSearchBar.js
+++ docusaurus/src/theme/DocSidebar/CustomSearchBar.js
@@ -5,7 +5,10 @@ import Icon from '../../components/Icon.js'
 export default function CustomSearchBarWrapper(props) {
   return (
     <div className="my-custom-search-bar">
-      <SearchBar {...props} />
+      <button type="button" className="DocSearch DocSearch-Button" aria-label="Search">
+        <span className="DocSearch-Button-Container">
+          <svg width="20" height="20" className="DocSearch-Search-Icon" viewBox="0 0 20 20" aria-hidden="true">
+            <path d="M14.386 14.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z" stroke="currentColor" fill="none" fillRule="evenodd" strokeLinecap="round" strokeLinejoin="round"></path></svg><span className="DocSearch-Button-Placeholder">Search</span></span><span className="DocSearch-Button-Keys"><kbd className="DocSearch-Button-Key">⌘</kbd><kbd className="DocSearch-Button-Key">K</kbd></span></button>
       <button className="kapa-widget-button"><span className="kapa-widget-button-text"><Icon name="sparkle"/>Ask AI</span></button>
     </div>
   );
