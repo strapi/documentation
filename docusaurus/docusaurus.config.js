da1a31b7c2aadc9623f1ee2be5367961addbaea3
Pierre Wizla <pwizla+github@gmail.com>
2025-04-01 12:00:17 +0200

    Remove facetFilters from Algolia config.

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -191,9 +191,9 @@ const config = {
          appId: '392RJ63O14',
          apiKey: '2470d74a1f395f5017994e7b17a77bf8',
          indexName: 'strapi_newCmsCrawler_march2025',
-         searchParameters: {
-          facetFilters: []
-         },
+        //  searchParameters: {
+        //   facetFilters: []
+        //  },
       },
       navbar: {
         hideOnScroll: false,
