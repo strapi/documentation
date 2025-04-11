c0af169bf0fb0726fcb2580aa32c3e46f1fbc73c
Pierre Wizla <pwizla+github@gmail.com>
2025-04-11 15:15:01 +0200

    Change approach and swizzle TOCItems to preserver sticky behavior

--- docusaurus/src/theme/TOC/index.js
+++ docusaurus/src/theme/TOC/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import TOC from '@theme-original/TOC';
-import ContributeLink from '../../components/ContributeLink';
 
 export default function TOCWrapper(props) {
   return (
     <>
       <TOC {...props} />
-      <ContributeLink />
     </>
   );
 }
