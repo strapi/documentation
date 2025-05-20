1e728838ac25871de9b05eb56a4846b63391629b
Pierre Wizla <pwizla+github@gmail.com>
2025-05-20 19:57:54 +0200

    Algolia Search

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -189,7 +189,7 @@ const config = {
       },
       algolia: {
         appId: '392RJ63O14',
-        apiKey: '2470d74a1f395f5017994e7b17a77bf8',
+        apiKey: '3f4b8953a20a4c5af4614a607ecf9a93',
         indexName: 'strapi_newCmsCrawler_march2025',
         contextualSearch: false,
         searchParameters: {
