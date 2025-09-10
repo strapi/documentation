2733bf4777a259ce52fab6d5c691161a0d5dc88f
Pierre Wizla <pwizla+github@gmail.com>
2025-09-10 16:00:51 +0200

    WIP

--- docusaurus/src/theme/DocSidebar/CustomSearchBar.js
+++ docusaurus/src/theme/DocSidebar/CustomSearchBar.js
@@ -5,7 +5,7 @@ import Icon from '../../components/Icon.js'
 export default function CustomSearchBarWrapper(props) {
   return (
     <div className="my-custom-search-bar">
-      <SearchBar {...props} />
+      <div id="docsearch"></div>
       <button className="kapa-widget-button"><span className="kapa-widget-button-text"><Icon name="sparkle"/>Ask AI</span></button>
     </div>
   );
