26ad431c1baffab1e4094a1684eb34b9ef2c7739
Pierre Wizla <pwizla+github@gmail.com>
2025-09-23 15:41:00 +0200

    Tidy up CustomSearchBar.js code

--- docusaurus/src/theme/DocSidebar/CustomSearchBar.js
+++ docusaurus/src/theme/DocSidebar/CustomSearchBar.js
@@ -6,7 +6,11 @@ export default function CustomSearchBarWrapper(props) {
   return (
     <div className="my-custom-search-bar">
       <SearchBar {...props} />
-      <button className="kapa-widget-button"><span className="kapa-widget-button-text"><Icon name="sparkle"/>Ask AI</span></button>
+      <button className="kapa-widget-button">
+        <span className="kapa-widget-button-text">
+          <Icon name="sparkle"/>Ask AI
+        </span>
+      </button>
     </div>
   );
 }
