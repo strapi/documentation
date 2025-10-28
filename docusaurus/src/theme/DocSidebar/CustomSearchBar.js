08efc4025449a16142eb2dde34c3f86240bd4e77
Pierre Wizla <pwizla+github@gmail.com>
2025-10-28 11:46:19 +0100

    Add or update MeiliSearch-related front-end files

--- docusaurus/src/theme/DocSidebar/CustomSearchBar.js
+++ docusaurus/src/theme/DocSidebar/CustomSearchBar.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import SearchBar from '@theme-original/SearchBar';
+import SearchBar from '../SearchBar/index.js';
 import Icon from '../../components/Icon.js'
 
 export default function CustomSearchBarWrapper(props) {
   return (
     <div className="my-custom-search-bar">
       <SearchBar {...props} />
-      <button className="kapa-widget-button"><span className="kapa-widget-button-text"><Icon name="sparkle"/>Ask AI</span></button>
+      <button className="kapa-widget-button">
+        <span className="kapa-widget-button-text">
+          <Icon name="sparkle"/>Ask AI
+        </span>
+      </button>
     </div>
   );
 }
