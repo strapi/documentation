9c651d3f189ab2d67de07826304cfa2ac96ef7ef
Pierre Wizla <4233866+pwizla@users.noreply.github.com>
2025-11-17 15:23:26 +0100

    Add info for Vite server re. allowed hosts

--- docusaurus/src/components/SideBySide/SideBySideContainer/SideBySideContainer.jsx
+++ docusaurus/src/components/SideBySide/SideBySideContainer/SideBySideContainer.jsx
@@ -5,36 +5,22 @@ import styles from './side-by-side-container.module.scss';
 export function SideBySideContainer({ className, spaced = true, ...rest }) {
   return (
     <>
-      {/* Styles to increase the page-content width */}
       <style
         id="side-by-side-container--styles-overrides"
         dangerouslySetInnerHTML={{
           __html: `
-            /**
-             * Overrides applied by the component 'SideBySideContainer'
-             */
             @media (min-width: 997px) {
-
-              /**
-               * Increase the general container size
-               */
               main article:first-child:not(.col),
               main article:first-child:not(.col) + nav {
-                --custom-main-width: 120rem;
+                max-width: 1200px !important;
               }
             }
 
             @media (max-width: 1536px) {
-              /**
-               * Hide the right-side nav (table of contents)
-               */
               .container > .row > .col.col--3 {
                 display: none;
               }
 
-              /**
-               * Increase the content-column size
-               */
               .container > .row > .col:first-child {
                 max-width: 100% !important;
               }
