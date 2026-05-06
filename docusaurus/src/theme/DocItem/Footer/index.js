38a622e63ace17e89ec8f1c08c4bdafeea65c9ab
Pierre Wizla <4233866+pwizla@users.noreply.github.com>
2026-05-06 16:16:09 +0200

    Wire PageFeedback widget into DocItem/Footer swizzle

--- docusaurus/src/theme/DocItem/Footer/index.js
+++ docusaurus/src/theme/DocItem/Footer/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import Footer from '@theme-original/DocItem/Footer';
+import PageFeedback from '@site/src/components/PageFeedback';
+import { useDoc } from '@docusaurus/plugin-content-docs/client';
 
 export default function FooterWrapper(props) {
+  const { metadata } = useDoc();
+
   return (
     <>
+      <PageFeedback
+        pagePath={metadata.permalink}
+        pageId={metadata.id}
+        pageTitle={metadata.title}
+      />
       <Footer {...props} />
     </>
   );
