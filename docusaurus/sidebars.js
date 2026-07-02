a8e4dafcd4177eabbf25d09508233332ea0873f3
Pierre Wizla <pwizla+github@gmail.com>
2026-07-02 16:15:14 +0200

    Revert sidebar line deletion (2)

--- docusaurus/sidebars.js
+++ docusaurus/sidebars.js
@@ -345,6 +345,7 @@ const sidebars = {
         {
           type: 'category',
           label: 'Guides',
+          link: { type: 'generated-index', title: 'Configuration guides', slug: '/cms/configurations/guides' },
           items: [
             'cms/configurations/guides/access-cast-environment-variables',
             'cms/configurations/guides/access-configuration-values',
